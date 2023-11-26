4c6321b30a4cf1dfc1fcef24c9398dbcda2c006c
kkulesz <kulesza.konrad@protonmail.com>
2023-11-26 16:32:47 +0100

    minor - check out torch and cuda properties

--- tmp_main.py
+++ tmp_main.py
@@ -1,9 +1,26 @@
 import torch
 
 
+def print_machine_props():
+    print(f"Torch version: {torch.__version__}")
+    print(f"Is Cuda available: {torch.cuda.is_available()}")
+    gpus = torch.cuda.device_count()
+    print(f"Number of GPUs available: {gpus}")
+    for i in range(gpus):
+        def to_gb(bytes):
+            return round(bytes / 1024 / 1024 / 1024, 3)
+
+        t = torch.cuda.get_device_properties(i).total_memory
+        r = torch.cuda.memory_reserved(i)
+        a = torch.cuda.memory_allocated(i)
+        print(f"\tdevice nr {i}:")
+        print(f"\ttotal: \t\t{to_gb(t)} GB")
+        print(f"\treserved: \t{to_gb(r)} GB")
+        print(f"\tallocated: \t{to_gb(a)} GB")
+
+
 def main():
-    print(torch.cuda.is_available())
-    print(torch.__version__)
+    print_machine_props()
 
 
 if __name__ == '__main__':
