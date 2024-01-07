14a9e71d81356772be08875b0c8dacaf3e4e72f7
kkulesz <kulesza.konrad@protonmail.com>
2024-01-07 18:07:24 +0100

    stargan - figure out how to use saved models

--- tmp_main.py
+++ tmp_main.py
@@ -2,6 +2,7 @@ import torch
 from tqdm import tqdm
 import time
 
+
 def print_machine_props():
     print(f"Torch version: {torch.__version__}")
     print(f"Is Cuda available: {torch.cuda.is_available()}")
@@ -21,7 +22,7 @@ def print_machine_props():
 
 
 def test_tqdm():
-    l = range(0,10)
+    l = range(0, 10)
     s = 0
     for i, a in enumerate(tqdm(l)):
         time.sleep(1)
@@ -29,9 +30,29 @@ def test_tqdm():
     print(s)
 
 
+def test_state_dict():
+    # checkpoint_path = "StarGANv2-VC/Models/TEST/final_00001_epochs.pth"
+    # state_dict = torch.load(checkpoint_path, map_location="cpu")
+    #
+    # print(state_dict.keys())
+    # print(state_dict['model_ema'].keys())
+    # print(state_dict['model'].keys())
+    #
+    # generator = state_dict['model_ema']['generator']
+    # stargan = state_dict['model_ema']
+    #
+    # torch.save(generator, "generator.pth")
+    # torch.save(stargan, "stargan.pth")
+
+    checkpoint_path = "StarGANv2-VC/Models/epoch_00150.pth"
+    state_dict = torch.load(checkpoint_path, map_location="cpu")
+    # print(state_dict.keys())
+
 def main():
     # print_machine_props()
-    test_tqdm()
+    # test_tqdm()
+    test_state_dict()
+
 
 if __name__ == '__main__':
     main()
