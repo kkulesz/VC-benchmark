6746225e2e8ef541724d51956547bfc3d7b37137
kkulesz <kulesza.konrad@protonmail.com>
2024-01-12 20:14:22 +0100

    stargan - start training once again, save checkpoints outside of project directory, so they do not get destroyed by pycharm's server deployment

--- StarGANv2-VC/train.py
+++ StarGANv2-VC/train.py
@@ -144,6 +144,7 @@ def main(config_path):
         logger.info(f"eval : {get_log_results_str(eval_results)}")
 
         if (epoch % save_freq) == 0:
+            print("Saving...")
             trainer.save_checkpoint(osp.join(log_dir, 'epoch_%05d.pth' % epoch))
     trainer.save_checkpoint(osp.join(log_dir, 'final_%05d_epochs.pth' % epochs))
     return 0
