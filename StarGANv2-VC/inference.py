1487391ff6a6afa86ee5e3f52d7f3b96b1b50227
kkulesz <kulesza.konrad@protonmail.com>
2024-01-17 20:06:21 +0100

    stargan - cnt. refactorization of convert script

--- StarGANv2-VC/inference.py
+++ StarGANv2-VC/inference.py
@@ -7,12 +7,11 @@ from torch import nn
 import torch.nn.functional as F
 import torchaudio
 import librosa
+import time
 
 from Utils.ASR.models import ASRCNN
 from Utils.JDC.model import JDCNet
 from models import Generator, MappingNetwork, StyleEncoder
-
-speakers = [273]
 
 device = 'cuda'
 
@@ -43,9 +42,9 @@ def __build_model(model_params):
 
 def __compute_style(starganv2, speaker_dicts):
     reference_embeddings = {}
-    for key, (path, speaker) in speaker_dicts.items():
+    for speaker_id, (path, speaker_idx) in speaker_dicts.items():
         if path == "":
-            label = torch.LongTensor([speaker]).to(device)
+            label = torch.LongTensor([speaker_idx]).to(device)
             latent_dim = starganv2.mapping_network.shared[0].in_features
             ref = starganv2.mapping_network(torch.randn(1, latent_dim).to(device), label)
         else:
@@ -56,19 +55,20 @@ def __compute_style(starganv2, speaker_dicts):
             mel_tensor = __preprocess(wave).to(device)
 
             with torch.no_grad():
-                label = torch.LongTensor([speaker])
+                label = torch.LongTensor([speaker_idx])
                 ref = starganv2.style_encoder(mel_tensor.unsqueeze(1), label)
-        reference_embeddings[key] = (ref, label)
+        reference_embeddings[speaker_id] = (ref, label)
 
     return reference_embeddings
 
 
-def display(wave, path):
+def display(wave, filename):
     from scipy.io.wavfile import write
     import os
     dir_to_save_samples = "../../samples"
     os.makedirs(dir_to_save_samples, exist_ok=True)
-    write(os.path.join(dir_to_save_samples, f"{path}.wav"), 24000, wave)
+    print(filename)
+    write(os.path.join(dir_to_save_samples, f"{filename}.wav"), 24000, wave)
 
 
 def load_vocoder(vocoder_path: str):
@@ -103,78 +103,61 @@ def load_f0_model(fo_model_path: str):
     return f0_model
 
 
-def convert(f0_model, starganv2, vocoder):
-    selected_speakers = [273]
-    k = random.choice(selected_speakers)
-    wav_path = 'Demo/VCTK-corpus/p' + str(k) + '/p' + str(k) + '_023.wav'
-    audio, source_sr = librosa.load(wav_path, sr=24000)
-    audio = audio / np.max(np.abs(audio))
-    audio.dtype = np.float32
+def convert(f0_model, starganv2, vocoder, source, targets):
+    source_speaker_id, source_wav_path = source
+    source_audio, _ = librosa.load(source_wav_path, sr=24000)
+    source_audio = source_audio / np.max(np.abs(source_audio))
+    source_audio.dtype = np.float32
 
-    speaker_dicts = {}
-    for s in selected_speakers:
-        k = s
-        speaker_dicts['p' + str(s)] = ('Demo/VCTK-corpus/p' + str(k) + '/p' + str(k) + '_023.wav', speakers.index(s))
-    reference_embeddings = __compute_style(starganv2, speaker_dicts)
-
-    import time
+    target_speakers_dict = {}  # speaker_id -> (path_to_speaker_wav_file, speaker_index)
+    for idx, (spk, pth) in enumerate(targets):
+        target_speakers_dict[spk] = (pth, idx)
+    # speaker_id -> (speaker_embedding, speaker_index)
+    targets_reference_embeddings = __compute_style(starganv2, target_speakers_dict)
     start = time.time()
 
-    source = __preprocess(audio).to(device)
-    keys = []
+    source_audio = __preprocess(source_audio).to(device)
     converted_samples = {}
     reconstructed_samples = {}
-    converted_mels = {}
-
-    for key, (ref, _) in reference_embeddings.items():
+    for key, (ref, _) in targets_reference_embeddings.items():
         with torch.no_grad():
-            f0_feat = f0_model.get_feature_GAN(source.unsqueeze(1))
-            out = starganv2.generator(source.unsqueeze(1), ref, F0=f0_feat)
+            f0_feat = f0_model.get_feature_GAN(source_audio.unsqueeze(1))
+            stargan_out = starganv2.generator(source_audio.unsqueeze(1), ref, F0=f0_feat)
 
-            c = out.transpose(-1, -2).squeeze().to(device)
-            y_out = vocoder.inference(c)
+            stargan_out_reshaped = stargan_out.transpose(-1, -2).squeeze().to(device)
+            y_out = vocoder.inference(stargan_out_reshaped)
             y_out = y_out.view(-1).cpu()
 
-            if key not in speaker_dicts or speaker_dicts[key][0] == "":
-                recon = None
-            else:
-                wave, sr = librosa.load(speaker_dicts[key][0], sr=24000)
-                mel = __preprocess(wave)
-                c = mel.transpose(-1, -2).squeeze().to(device)
-                recon = vocoder.inference(c)
-                recon = recon.view(-1).cpu().numpy()
+            raw_target_wave, _ = librosa.load(target_speakers_dict[key][0], sr=24000)
+            raw_target_mel = __preprocess(raw_target_wave)
+            raw_target_mel_reshaped = raw_target_mel.transpose(-1, -2).squeeze().to(device)
+            recon = vocoder.inference(raw_target_mel_reshaped)
+            recon = recon.view(-1).cpu().numpy()
 
         converted_samples[key] = y_out.numpy()
         reconstructed_samples[key] = recon
 
-        converted_mels[key] = out
-
-        keys.append(key)
     end = time.time()
-    print('total processing time: %.3f sec' % (end - start))
+    print('total processing time: %.3f sec\n\n' % (end - start))
 
-    for key, wave in converted_samples.items():
-        print('Converted: %s' % key)
-        display(wave, "Converted")
-        print('Reference (vocoder): %s' % key)
-        if reconstructed_samples[key] is not None:
-            display(reconstructed_samples[key], "Reference (vocoder)")
+    for target_speaker_id, wave in converted_samples.items():
+        display(wave, f"converted_{source_speaker_id}_to_{target_speaker_id}")
+        display(reconstructed_samples[target_speaker_id], f'reconstructed_target_{target_speaker_id}_by_vocoder')
 
-    print('Original (vocoder):')
-    wave, sr = librosa.load(wav_path, sr=24000)
+    wave, sr = librosa.load(source_wav_path, sr=24000)
     mel = __preprocess(wave)
     c = mel.transpose(-1, -2).squeeze().to(device)
     with torch.no_grad():
         recon = vocoder.inference(c)
         recon = recon.view(-1).cpu().numpy()
-    display(recon, 'Original (vocoder)')
-    print('Original:')
-    display(wave, "Original")
+
+    display(recon, f'reconstructed_source_{source_speaker_id}_by_vocoder')
+    display(wave, f'original_{source_speaker_id}')
 
 
 def main():
     stargan_config_path = "Models/config.yml"
-    stargan_path = "../../Models/demodata/epoch_00150.pth"
+    stargan_path = "../../Models/demodata/final_00200_epochs.pth"
     f0_model_path = "Utils/JDC/bst.t7"
     vocoder_path = "Vocoder/checkpoint-400000steps.pkl"
 
@@ -182,7 +165,14 @@ def main():
     stargan = load_stargan(stargan_path, stargan_config_path)
     vocoder = load_vocoder(vocoder_path)
 
-    convert(f0_model, stargan, vocoder)
+    source = ('p273', 'Demo/VCTK-corpus/p273/p273_023.wav')
+    target_files = [
+        ('p230', 'Demo/VCTK-corpus/p230/p230_023.wav'),
+        ('p236', 'Demo/VCTK-corpus/p236/p236_023.wav'),
+        ('p259', 'Demo/VCTK-corpus/p259/p259_023.wav')
+    ]
+
+    convert(f0_model, stargan, vocoder, source, target_files)
 
 
 if __name__ == '__main__':
