bd32b772e8d5638e39d0fb16ecf5222d88dc76b7
kkulesz <kulesza.konrad@protonmail.com>
2024-04-02 10:17:25 +0200

    visualization - visualize MOSNet score; merge male and female results into one

--- visualization/plot_mosnet.py
+++ visualization/plot_mosnet.py
@@ -1,38 +1,14 @@
 import os
 import re
-import json
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import itertools
-import statistics
+
 
 def read_results(root_directory: str):
-    def merge_male_and_female_results(dataframe: pd.DataFrame):
-        models = dataframe['model'].unique()
-        speakers = dataframe['number_of_speakers'].unique()
-        seens = dataframe['seen'].unique()
-        results_merged = []
-        for m, spks, s in itertools.product(models, speakers, seens):
-            filtered = dataframe.loc[
-                (dataframe['model'] == m) &
-                (dataframe['number_of_speakers'] == spks) &
-                (dataframe['seen'] == s)
-            ]
-
-            assert len(filtered) == 2
-            mcd = statistics.mean(filtered['MCD'])
-            snr = statistics.mean(filtered['SNR'])
-            merged = (m, spks, s, mcd, snr)
-
-            results_merged.append(merged)
-        final = pd.DataFrame(results_merged,
-                          columns=['model', 'number_of_speakers', 'seen', 'MCD', 'SNR'])
-        return final
-
     results = []
     for root_dir, dirs, files in os.walk(root_directory):
-        if 'results.json' in files:
+        if 'MOSnet_result_raw.txt' in files:
 
             if 'stargan' in root_dir:
                 model = 'stargan'
@@ -43,26 +19,19 @@ def read_results(root_directory: str):
             number_of_speakers = int(re.search(r'\d+', re.search(r'\d+spks', root_dir).group()).group())
             seen = 'unseen' not in root_dir
             directory = os.path.basename(root_dir)
-            male_to_female = directory in ['VCC2TM2_in_voice_of_VCC2TF2', 'VCC2SM1_in_voice_of_VCC2SF1']
-            with open(os.path.join(root_dir, 'results.json')) as f:
-                metric_results = json.load(f)
+            with open(os.path.join(root_dir, 'MOSnet_result_raw.txt')) as f:
+                mosnet_score = float(re.search(r'\d+.\d+', re.search(r'Average: \d+.\d+', f.read()).group()).group())
 
             results.append(
-                (model, number_of_speakers, seen, directory, male_to_female,
-                 metric_results['MCD'],
-                 metric_results['SNR'])
+                (model, number_of_speakers, seen, directory, mosnet_score)
             )
     df = pd.DataFrame(results,
-                      columns=['model', 'number_of_speakers', 'seen', 'directory', 'male_to_female', 'MCD', 'SNR'])
-
-    df = merge_male_and_female_results(df)
-
+                      columns=['model', 'number_of_speakers', 'seen', 'directory', 'mosnet_score'])
     return df
 
 
 def filter_results_and_reformat_it(
         seen: bool,
-        metric: str,
         df: pd.DataFrame,
         plot_title: str
 ):
@@ -74,20 +43,15 @@ def filter_results_and_reformat_it(
     df_stargan = df_stargan.sort_values(['number_of_speakers'])
     df_triann = df_triann.sort_values(['number_of_speakers'])
 
-    metric_stargan = df_stargan[metric].tolist()
-    metric_triann = df_triann[metric].tolist()
+    metric_stargan = df_stargan['mosnet_score'].tolist()
+    metric_triann = df_triann['mosnet_score'].tolist()
 
     min_value = min(metric_triann + metric_stargan)
     max_value = max(metric_triann + metric_stargan)
-    if metric == 'MCD':
-        y_limits = [5, 8]
-    elif metric == 'SNR':
-        y_limits = [0, 2.7]
-    else:
-        y_limits = (0.8 * min_value, 1.05 * max_value)
+    y_limits = (2.5, 5)
 
     x_ticks = df_stargan['number_of_speakers'].tolist()
-    y_label = metric
+    y_label = 'MOSNet score'
     models_plot_properties = [
         ('StarGANv2-VC', 'red', metric_stargan),
         ('TriANN-VC', 'blue', metric_triann)
@@ -96,7 +60,7 @@ def filter_results_and_reformat_it(
     return plot_title, y_label, x_ticks, models_plot_properties, y_limits
 
 
-def bar_plot_of_metric_over_number_of_speakers_for_each_model(
+def bar_plot_of_MOSNet_score_over_number_of_speakers(
         plot_title: str,
         y_label: str,
         x_ticks,
--- visualization/visualize.py
+++ visualization/visualize.py
@@ -4,6 +4,7 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import glob
 
 MCD = 'MCD'
 SNR = 'SNR'
@@ -12,16 +13,15 @@ SNR = 'SNR'
 def plot_metrics():
     import plot_metrics
     seen = True
-    male_to_female = True
-    metric = SNR
+    metric = MCD
 
     df = plot_metrics.read_results('../../samples')
     plot_title, y_label, x_ticks, models_plot_properties, bottom = \
-        plot_metrics.filter_results_and_reformat_it(seen=seen, male_to_female=male_to_female, metric=metric, df=df,
+        plot_metrics.filter_results_and_reformat_it(seen=seen, metric=metric, df=df,
                                                     plot_title=f'{metric} over number of {"seen" if seen else "unseen"} speakers in training dataset')
 
-    plot_metrics.bar_plot_of_metric_over_number_of_speakers_for_each_model(plot_title, y_label, x_ticks,
-                                                                           models_plot_properties, bottom)
+    plot_metrics.bar_plot_of_metric_over_number_of_speakers_for_each_model(
+        plot_title, y_label, x_ticks, models_plot_properties, bottom)
 
 
 def plot_execution_time():
@@ -33,9 +33,33 @@ def plot_execution_time():
     plot_execution_time.bar_plot_of_execution_time(plot_title, y_label, x_ticks, models_plot_properties)
 
 
+def plot_mosnet_score():
+    import plot_mosnet
+    seen = True
+
+    df = plot_mosnet.read_results('../../samples')
+    print(df)
+    plot_title, y_label, x_ticks, models_plot_properties, bottom = \
+        plot_mosnet.filter_results_and_reformat_it(seen=seen, df=df,
+                                                   plot_title=f'MOSNet score over number of {"seen" if seen else "unseen"} speakers in training dataset')
+
+    plot_mosnet.bar_plot_of_MOSNet_score_over_number_of_speakers(
+        plot_title, y_label, x_ticks, models_plot_properties, bottom)
+
+
+def remove_mosnet_results(directory: str = '../../samples'):
+    files_pattern = os.path.join(directory, '*/*/*/*/MOSnet_result_raw.txt')
+    files_to_remove = glob.glob(files_pattern)
+    for f in files_to_remove:
+        os.remove(f)
+
+
 def main():
     plot_metrics()
     # plot_execution_time()
+    # plot_mosnet_score()
+
+    # remove_mosnet_results()
 
 
 if __name__ == '__main__':
